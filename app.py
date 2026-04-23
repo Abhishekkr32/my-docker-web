7b99c34b26db261ba398be6fc9f8382727dc4034
Abhishekkr <abhisaroj226002@gmail.com>
2026-04-23 12:30:40 +0530

    Full ci/cd depl proj

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Docker CI/CD!"
+    return "Hello from CI/CD Deployment!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
